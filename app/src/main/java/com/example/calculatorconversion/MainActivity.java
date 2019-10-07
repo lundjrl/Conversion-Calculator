02c83f132fa6326eef4d9154c1be63ffe5e2ae4e
zachary-thomas <thomazac@mail.gvsu.edu>
2019-10-07 16:19:15 -0400

    Add initial code

--- app/src/main/java/com/example/calculatorconversion/MainActivity.java
+++ app/src/main/java/com/example/calculatorconversion/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.calculatorconversion;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Fields
+        TextView textFieldFrom = (TextView) findViewById(R.id.textFieldFrom);
+        TextView textFieldTo = (TextView) findViewById(R.id.textFieldTo);
+
+        // Units
+        TextView converterUnit = (TextView) findViewById(R.id.converterUnit);
+        TextView fromUnitText = (TextView) findViewById(R.id.fromUnitText);
+        TextView toUnitText = (TextView) findViewById(R.id.toUnitText);
+
+        // Buttons
+        Button calculateButton = (Button) findViewById(R.id.calculateButton);
+        Button clearButton = (Button) findViewById(R.id.clearButton);
+        Button modeButton = (Button) findViewById(R.id.modeButton);
+
+        clearButton.setOnClickListener( e -> {
+            textFieldFrom.setText("");
+            textFieldTo.setText("");
+        });
+
+        calculateButton.setOnClickListener( e -> {
+            double fromNum = Double.parseDouble(textFieldFrom.getText().toString());
+            double toNum = Double.parseDouble(textFieldTo.getText().toString());
+        });
+
+
+
     }
+
 }
